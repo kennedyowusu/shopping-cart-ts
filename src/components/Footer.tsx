48dbb473e75bf172ec3029522bc7ba1a66644c51
Kennedy Owusu <kennediowusu@gmail.com>
2023-08-12 20:50:25 +0000

    add github link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,9 @@ const Footer = () => {
     <footer className='flex justify-between items-center text-slate-500 h-16 w-full'>
       <div className='flex  justify-between items-center w-full container py-4 px-2 mx-auto'>
         <div className='text-xl font-bold'>© 2023 Shopping Cart</div>
-        <div className='text-xl font-bold'>Kennedy</div>
+        <div className='text-xl font-bold'>
+          <a href='https://github.com/kennedyowusu'>Kennedy</a>
+        </div>
       </div>
     </footer>
   )
